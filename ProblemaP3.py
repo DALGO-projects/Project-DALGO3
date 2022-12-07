04b2bbeab504eff58b018b75269eb1473d5acb0e
santyxd3000 <s.pardom@uniandes.edu.co>
2022-12-06 22:29:05 -0500

    Cambios en el archivo

--- ProblemaP3.py
+++ ProblemaP3.py
@@ -1,33 +1,34 @@
 
-
 def saltoDivisor(m:int,k:int):
-      #Inicializar listas
-      lista = [0 for _ in range(m+1)]
-      totals = [0 for _ in range(m+1)]
       modulo = 998244353
-
-      for i in range(m//k):
-            lista[(i+1)*k] = lista[(i+1)*k] + 1
-            totals[(i+1)*k] = totals[(i+1)*k] + 1
-      
-      sum = 2*k + 1
-
-      while sum <= m:
-            actual = [0 for _ in range(m+1)]
+      DP1 = [0 for _ in range(m+1)]
+      DP1[0] = 1
+      DP2 = [0 for _ in range(m+1)]
+      R = [0 for _ in range(m)]
+      conteo = 0
+      while conteo < m:
+            for i in range(conteo,m+1-k):
+                  DP2[i+k] = DP2[i+k] + (DP1[i]+DP2[i])%modulo
+                  DP2[i+k] = DP2[i+k]%modulo
+            DP1 = DP2
+            DP2 = [0 for _ in range(m+1)]
+            for i in range(conteo,m+1):
+                  R[i-1] = R[i-1]+DP1[i]
+                  R[i-1] = R[i-1]%modulo
+            conteo = conteo+k
             k = k+1
-            for i in range(k,m+1):
-                  actual[i] = (actual[i-k]+totals[i-k])%modulo
-                  lista[i] = (actual[i]+lista[i])%modulo
-            totals = actual
-            sum = sum + k
-      
-      longitud = len(lista)
-
-      return lista
+      return R[m-1]
 
 
-lista = saltoDivisor(25252,11)
-print(lista)
+#Casos de prueba
 
+"""
+rta1 = saltoDivisor(8,1)
+rta2 = saltoDivisor(10,2)
+rta3 = saltoDivisor(10000,3)
+rta4 = saltoDivisor(10000,7)
+rta5 = saltoDivisor(200000,1) #El que mas se demora
 
+print(rta)
+"""
             
