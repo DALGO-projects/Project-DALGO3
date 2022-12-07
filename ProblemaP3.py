6e61270a1cd5a7234444807a70571f31b702538b
JaimeAlfonsoR <j.alfonsor@uniandes.edu.co>
2022-12-07 11:46:58 -0500

    consola

--- ProblemaP3.py
+++ ProblemaP3.py
@@ -1,4 +1,6 @@
 
+import sys
+import time
 def saltoDivisor(m:int,k:int):
       modulo = 998244353
       DP1 = [0 for _ in range(m+1)]
@@ -31,4 +33,19 @@ rta5 = saltoDivisor(200000,1) #El que mas se demora
 
 print(rta)
 """
+def main():
+    linea = sys.stdin.readline() 
+    casos = int(linea)
+    linea = sys.stdin.readline()
+    sumaT = 0
+    for i in range(0,casos):
+       numeros = [int(num) for num in linea.split()]
+       start = (time.time())
+       respuesta = saltoDivisor(numeros[0], numeros[1])
+       end = (time.time())
+       sumaT = sumaT + (end - start) #Tiempo acumulado
+       print((respuesta))
+       linea = sys.stdin.readline()
+    print("Time: "+ str(sumaT))    
+main()
             
